01508e85d2953d4a66e7704c4f01e92022a5bd46
sat899 <satyam.bhagwanani@gmail.com>
2026-08-14 14:21:31 +0100

    improving comments and print statements for softmax function

--- src/modules/activations.py
+++ src/modules/activations.py
@@ -18,11 +18,20 @@ def softmax(scores, subtract_max = True):
     if subtract_max:
         scores = scores - np.max(scores, axis=-1, keepdims=True) # subtracting max value before exponentiating to prevent numerical overflow.
 
+    # numerator
     print('calculating the numerator = e^x for each value of x in the input tensor of scores')
     numerator = np.exp(scores) # np.exp() calculates e^x for each value of x in your input array/tensor. Element-wise, shape does not change.
-    print(f'the nummerator is {numerator}')
+    print(f'the numerator is {numerator.tolist()}')
+
+    # denominator
     print('calculating the denominator = summing across the last dimension of the numerator (vocab_size)')
     denominator = np.sum(numerator, axis=-1, keepdims=True) # sums across the last dimension which is the vocab size, so getting a probability distribution over all words in the vocab. Shape is [batch_size, seq_len, 1]
-    print(f'the denominator is {denominator}')
-    prob = numerator / denominator # same shape as scores
-    return prob
+    print(f'the denominator is {denominator.tolist()}')
+
+    # probs
+    print(f'calculating the probabilities by dividing the numerator by the denominator')
+    probs = numerator / denominator # same shape as scores
+    print(f'the final probabilities are {probs.tolist()}')
+    print(f'the shape of the probabilities is {probs.shape}')
+
+    return probs
