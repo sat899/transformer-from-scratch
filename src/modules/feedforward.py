72146f72e521a144d3f3c6362de8662d9f9627cf
sat899 <satyam.bhagwanani@gmail.com>
2026-08-29 09:36:29 +0100

    clarifying dimension requirements in docstrings

--- src/modules/feedforward.py
+++ src/modules/feedforward.py
@@ -15,10 +15,12 @@ def linear_layer(input, weights, bias=None):
     y = xw + b
     can use either np.matmul() or numpy's built in matrix multiplication operator here (@), they are equivalent
 
-    input has shape [n_inputs, n_input_features]
-    weights has shape [n_input_features, n_output_classes]
-    bias has shape [n_output_classes, 1]
-    output has shape [n_inputs, n_output_classes]
+    input has shape [n_inputs, input_size]
+    weights has shape [input_size, output_size]
+    bias has shape [1, output_size]
+    output has shape [n_inputs, output_size]
+    labels_sparse has shape [n_inputs]
+    labels_one_hot has shape [n_inputs, output_size]
 
     """
     # note they must be multiplued in this order so that the inner dimensions match up
