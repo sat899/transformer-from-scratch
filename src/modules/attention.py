5dab326e8c4cae649fca5008c6028f96d3131228
sat899 <satyam.bhagwanani@gmail.com>
2026-09-18 17:34:36 +0100

    adding function to compute attention scores

--- src/modules/attention.py
+++ src/modules/attention.py
@@ -22,15 +22,26 @@ def generate_qkv_weight_matrices(embedding_dim: int, head_dim: int) -> tuple[np.
 
     return query_weight_matrix, key_weight_matrix, value_weight_matrix
 
-
 def generate_qkv_matrices(input_embedding: np.ndarray, query_weight_matrix: np.ndarray, key_weight_matrix: np.ndarray, value_weight_matrix: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Calculates the query, key and value matrices from the input embedding and weight matrices
     Input embedding has size [seq_len, embed_dim]
     Weight matrices have size [embed_dim, head_dim]
+    Resulting matrices have size [seq_len, head_dim]
     """
     query_matrix = np.matmul(input_embedding, query_weight_matrix)
     key_matrix = np.matmul(input_embedding, key_weight_matrix)
     value_matrix = np.matmul(input_embedding, value_weight_matrix)
 
     return query_matrix, key_matrix, value_matrix
+
+def calculate_attention_scores(query_matrix: np.ndarray, key_matrix: np.ndarray) -> np.ndarray:
+    """
+    Calculates attention scores by multiplying the query matrix [seq_len, head_dim] with the transposed key matrix [head_dim, seq_len]
+    Resulting attention scores have shape [seq_len, seq_len]
+    """
+    qkt = np.matmul(query_matrix, key_matrix.T)
+    scaling_factor = np.sqrt(query_matrix.shape[1]) #square root of head_dim
+    attention_scores = qkt/scaling_factor
+
+    return attention_scores
